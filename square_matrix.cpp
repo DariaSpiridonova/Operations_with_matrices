30d3c03e8da5b30a00d7ea501fd346efaea9ebf1
DariaSpiridonova <spiridonova.da@phystech.edu>
2025-09-13 22:50:18 +0300

    add asserts and change name of func

--- square_matrix.cpp
+++ square_matrix.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
-int *func(int *matrix, size_t size_x, int x, int y);
+int *get_ptr_on_element_of_matrix(int *matrix, size_t size_x, int x, int y);
 struct matrix_parameters get_matrix(void);
 void print_matrix(int *matrix, int y, int x, size_t n);
 struct min_and_max_items get_max_and_min_item(int *matrix, int y, int x);
@@ -39,8 +40,10 @@ int main()
     print_matrix(product_of_matrices, matrix1_par.y, matrix2_par.x, 3);
 }
 
-int *func(int *matrix, size_t size_x, int x, int y)
+int *get_ptr_on_element_of_matrix(int *matrix, size_t size_x, int x, int y)
 {
+    assert(matrix != NULL);
+
     return &matrix[size_x * y + x];
 }
 
@@ -55,12 +58,9 @@ struct matrix_parameters get_matrix(void)
     printf("Введите %d строк матрицы, разделяя элементы пробелом:\n", y);
     int *matrix = (int *)calloc(x * y, sizeof(int));
 
-    for (int i = 0; i < y; i++)
+    for (int i = 0; i < y * x; i++)
     {
-        for (int j = 0; j < x; j++)
-        {
-            scanf("%d", func(matrix, x, j, i));
-        }
+        scanf("%d", &matrix[i]);
     }
     matrix_n.matrix = matrix;
     matrix_n.y = y;
@@ -71,12 +71,14 @@ struct matrix_parameters get_matrix(void)
 
 void print_matrix(int *matrix, int y, int x, size_t n)
 {
+    assert(matrix != NULL);
+
     printf("Матрица №%d\n", n);
     for (int i = 0; i < y; i++)
     {
         for (int j = 0; j < x; j++)
         {
-            printf("%d ", *func(matrix, x, j, i));
+            printf("%d ", *get_ptr_on_element_of_matrix(matrix, x, j, i));
         }
         printf("\n");
     }
@@ -84,19 +86,18 @@ void print_matrix(int *matrix, int y, int x, size_t n)
 
 struct min_and_max_items get_max_and_min_item(int *matrix, int y, int x)
 {
+    assert(matrix != NULL);
+
     struct min_and_max_items extreme_elements;
     int min_item = *matrix;
     int max_item = *matrix;
 
-    for (int i = 0; i < y; i++)
+    for (int i = 0; i < y * x; i++)
     {
-        for (int j = 0; j < x; j++)
-        {
-            if (*func(matrix, x, j, i) > max_item)
-                max_item = *func(matrix, x, j, i);
-            if (*func(matrix, x, j, i) < min_item)
-                min_item = *func(matrix, x, j, i);
-        }
+        if (matrix[i] > max_item)
+            max_item = matrix[i];
+        if (matrix[i] < min_item)
+            min_item = matrix[i];
     }
 
     extreme_elements.min_item = min_item;
@@ -107,6 +108,8 @@ struct min_and_max_items get_max_and_min_item(int *matrix, int y, int x)
 
 void print_max_and_min_item(struct min_and_max_items extreme_elements, size_t n)
 {
+    assert(&extreme_elements != NULL);
+
     printf("Максимальный элемент матрицы №%d = %d\n", n, extreme_elements.max_item);
     printf("Минимальный элемент матрицы №%d = %d\n", n, extreme_elements.min_item);
 }
@@ -119,11 +122,15 @@ int *create_product_of_matrices(int y1, int x2)
 
 int *get_product_of_matrices(int *matrix_product, int *matrix1, int *matrix2, int y2, int y1, int x2)
 {
+    assert(matrix_product != NULL);
+    assert(matrix1 != NULL);
+    assert(matrix2 != NULL);
+
     for (int i = 0; i < y1; i++)
     {
         for (int j = 0; j < x2; j++)
         {
-            *func(matrix_product, x2, j, i) = get_single_element_of_product_matrix(matrix1, matrix2, y2, x2, i, j);
+            *get_ptr_on_element_of_matrix(matrix_product, x2, j, i) = get_single_element_of_product_matrix(matrix1, matrix2, y2, x2, i, j);
         }
     }
 
@@ -132,10 +139,13 @@ int *get_product_of_matrices(int *matrix_product, int *matrix1, int *matrix2, in
 
 int get_single_element_of_product_matrix(int *matrix1, int *matrix2, int y2, int x2, int i, int j)
 {
+    assert(matrix1 != NULL);
+    assert(matrix2 != NULL);
+
     int value_of_single_element = 0;
     for (int t = 0; t < y2; t++)
     {
-        value_of_single_element += (*func(matrix1, y2, t, i)) * (*func(matrix2, x2, j, t));
+        value_of_single_element += (*get_ptr_on_element_of_matrix(matrix1, y2, t, i)) * (*get_ptr_on_element_of_matrix(matrix2, x2, j, t));
     }
     return value_of_single_element;
 }
