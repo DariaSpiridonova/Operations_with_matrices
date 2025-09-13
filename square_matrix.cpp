115d9a9e3412993f58b3bf9ad9408c6781d78dce
DariaSpiridonova <spiridonova.da@phystech.edu>
2025-09-13 19:48:01 +0300

    add structures and wish size

--- square_matrix.cpp
+++ square_matrix.cpp
@@ -1,104 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void get_matrix(char* matrix, int y);
-void print_matrix(char matrix[][4], int y, size_t n);
-void print_max_and_min_item(char matrix[][4], int y, size_t n);
-void print_product_of_matrices(char matrix1[][4], char matrix2[][4], int y1, int y2);
-void item_value(char matrix1[][4], char matrix2[][4], int y2, int i, int j);
+struct matrix_parameters get_matrix(void);
+void print_matrix(int *matrix, int y, int x, size_t n);
+struct min_and_max_items get_max_and_min_item(int *matrix, int y, int x, size_t n);
+void print_max_and_min_item(struct min_and_max_items extreme_elements, size_t n);
+int *create_product_of_matrices(int y1, int x2);
+int *get_product_of_matrices(int *matrix_product, int *matrix1, int *matrix2, int y2, int y1, int x2);
+int get_single_element_of_product_matrix(int *matrix1, int *matrix2, int y2, int x2, int i, int j);
 
-// get_matrix -> int*, x, y (структура)
-
-
-// int* matrix, size_t size_x, size_y, x, y
-// matrix[size_x*y + x] = 5
-int * func(int *matrix, size_t size_x, int x, int y)
+struct matrix_parameters
 {
-    return &matrix[size_x*y + x];
-}
+    int *matrix;
+    int x;
+    int y;
+};
+
+struct min_and_max_items
+{
+    int min_item;
+    int max_item;
+};
 
 int main()
-{ 
-    char my_matrix1[4][4] = {};
-    get_matrix(my_matrix1, 4);
-    char my_matrix2[4][4] = {};
-    get_matrix(my_matrix2, 4);
+{
+    struct matrix_parameters matrix1_par = get_matrix();
+    struct matrix_parameters matrix2_par = get_matrix();
 
-    print_matrix(my_matrix1, 4, 1);
-    print_matrix(my_matrix2, 4, 2);
+    print_matrix(matrix1_par.matrix, matrix1_par.y, matrix1_par.x, 1);
+    print_matrix(matrix2_par.matrix, matrix2_par.y, matrix2_par.x, 2);
 
-    print_max_and_min_item(my_matrix1, 4, 1);
-    print_max_and_min_item(my_matrix2, 4, 2);
+    print_max_and_min_item(get_max_and_min_item(matrix1_par.matrix, matrix1_par.y, matrix1_par.x, 1), 1);
+    print_max_and_min_item(get_max_and_min_item(matrix2_par.matrix, matrix2_par.y, matrix2_par.x, 2), 2);
 
-    print_product_of_matrices(my_matrix1, my_matrix2, 4, 4);
+    int *product_of_matrices = get_product_of_matrices(create_product_of_matrices(matrix1_par.y, matrix2_par.x), matrix1_par.matrix, matrix2_par.matrix, matrix2_par.y, matrix1_par.y, matrix2_par.x);
+
+    print_matrix(product_of_matrices, matrix1_par.y, matrix2_par.x, 3);
 }
 
-void get_matrix(char matrix[][4], int y)
+int *func(int *matrix, size_t size_x, int x, int y)
 {
+    return &matrix[size_x * y + x];
+}
+
+struct matrix_parameters get_matrix(void)
+{
+    struct matrix_parameters matrix_n;
+    int y = 0, x = 0;
+    printf("Укажите количество строк и стобцов в матрице через пробел: ");
+    scanf("%d ", &y);
+    scanf("%d", &x);
+
     printf("Введите %d строк матрицы, разделяя элементы пробелом:\n", y);
-    char *str = (char *)calloc(10, sizeof(char));
+    int *matrix = (int *)calloc(x * y, sizeof(int));
 
     for (int i = 0; i < y; i++)
     {
-        fgets(str, 10, stdin);
-        sscanf(str, "%c %c %c %c %c", &matrix[i][0], &matrix[i][1], &matrix[i][2], &matrix[i][3], &matrix[i][4]);
+        for (int j = 0; j < x; j++)
+        {
+            scanf("%d", func(matrix, x, j, i));
+        }
     }
+    matrix_n.matrix = matrix;
+    matrix_n.y = y;
+    matrix_n.x = x;
+
+    return matrix_n;
 }
 
-void print_matrix(char matrix[][4], int y, size_t n)
+void print_matrix(int *matrix, int y, int x, size_t n)
 {
     printf("Матрица №%d\n", n);
     for (int i = 0; i < y; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < x; j++)
         {
-            printf("%c ", matrix[i][j]);
+            printf("%d ", *func(matrix, x, j, i));
         }
         printf("\n");
     }
 }
 
-void print_max_and_min_item(char matrix[][4], int y, size_t n)
+struct min_and_max_items get_max_and_min_item(int *matrix, int y, int x, size_t n)
 {
-    char min_item = matrix[0][0];
-    char max_item = matrix[0][0];
+    struct min_and_max_items extreme_elements;
+    char min_item = *matrix;
+    char max_item = *matrix;
 
     for (int i = 0; i < y; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < x; j++)
         {
-            if (matrix[i][j] > max_item) max_item = matrix[i][j];
-            if (matrix[i][j] < min_item) min_item = matrix[i][j];
+            if (*func(matrix, x, j, i) > max_item)
+                max_item = *func(matrix, x, j, i);
+            if (*func(matrix, x, j, i) < min_item)
+                min_item = *func(matrix, x, j, i);
         }
     }
 
-    printf("Максимальный элемент матрицы №%d = %c\n", n, max_item);
-    printf("Минимальный элемент матрицы №%d = %c\n", n, min_item);
+    extreme_elements.min_item = min_item;
+    extreme_elements.max_item = max_item;
+
+    return extreme_elements;
 }
 
-void print_product_of_matrices(char matrix1[][4], char matrix2[][4], int y1, int y2)
+void print_max_and_min_item(struct min_and_max_items extreme_elements, size_t n)
 {
-    printf("Произведение матриц 1 и 2 равно следующей матрице:\n");
+    printf("Максимальный элемент матрицы №%d = %d\n", n, extreme_elements.max_item);
+    printf("Минимальный элемент матрицы №%d = %d\n", n, extreme_elements.min_item);
+}
+
+int *create_product_of_matrices(int y1, int x2)
+{
+    int *matrix = (int *)calloc(y1 * x2, sizeof(int));
+    return matrix;
+}
+
+int *get_product_of_matrices(int *matrix_product, int *matrix1, int *matrix2, int y2, int y1, int x2)
+{
     for (int i = 0; i < y1; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < x2; j++)
         {
-            item_value(matrix1, matrix2, y2, i, j);
+            *func(matrix_product, x2, j, i) = get_single_element_of_product_matrix(matrix1, matrix2, y2, x2, i, j);
         }
-        printf("\n");
     }
+
+    return matrix_product;
 }
 
-void item_value(char matrix1[][4], char matrix2[][4], int y2, int i, int j)
+int get_single_element_of_product_matrix(int *matrix1, int *matrix2, int y2, int x2, int i, int j)
 {
-    int item_value = 0;
+    int value_of_single_element = 0;
     for (int t = 0; t < y2; t++)
     {
-        item_value += (((int)matrix1[i][t] - (int)'0') * ((int)matrix2[t][j] - (int)'0')); 
+        value_of_single_element += (*func(matrix1, y2, t, i)) * (*func(matrix2, x2, j, t));
     }
-    printf("%3d ", item_value);
+    return value_of_single_element;
 }
-
-void solve(double a, double b, double c);
-
-//функцияб возвращающая рез перемножения матриц, возвр новую матрицу, созд вне функции
